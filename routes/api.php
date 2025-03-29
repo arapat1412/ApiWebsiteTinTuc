aa4e5f5bb4fb5c86c3f86e6988aca4276dd320b5
Hua Vinh Thang <thanghuavinh@gmail.com>
2025-03-29 16:51:32 +0700

    sua merge conflict

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-<<<<<<< HEAD
 use App\Http\Controllers\ReturnDanhSachController;
 use App\Http\Controllers\TinController;
 use Illuminate\Support\Facades\Route;
@@ -9,13 +8,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/trangchu', [ReturnDanhSachController::class, 'getDuLieuChoTrangChu']);
 Route::get('/tintuc', [TinController::class, 'danhSachTin']);
 Route::get('/tin-tuc/{id}', [ChiTietTinTheoIDController::class, 'layChiTietTin']);
-
-
-
-=======
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\TinController;
-
-Route::get('/tintuc', [TinController::class, 'danhSachTin']);
-Route::get('/tin-tuc/{id}', [ChiTietTinTheoIDController::class, 'layChiTietTin']);
->>>>>>> ae9a47947d7891b786f4491533233c4136b68c23
