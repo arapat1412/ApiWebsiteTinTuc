ae9a47947d7891b786f4491533233c4136b68c23
DinhNguyen2011 <dinhn6666@gmail.com>
2025-03-29 16:05:52 +0700

    Search_tin_theoID

--- routes/api.php
+++ routes/api.php
@@ -4,3 +4,4 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TinController;
 
 Route::get('/tintuc', [TinController::class, 'danhSachTin']);
+Route::get('/tin-tuc/{id}', [ChiTietTinTheoIDController::class, 'layChiTietTin']);
