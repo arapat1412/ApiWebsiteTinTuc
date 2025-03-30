a1f6264afc9f8fe721886887adecd9b7535efede
DinhNguyen2011 <dinhn6666@gmail.com>
2025-03-30 21:23:08 +0700

    Id TinTuc chi tiet

--- app/Http/Controllers/ChiTietTinTheoIDContronller.php
+++ app/Http/Controllers/ChiTietTinTheoIDContronller.php
@@ -15,15 +15,29 @@ class ChiTietTinTheoIDController extends Controller
      */
     public function layChiTietTin($id)
     {
-        // Tìm tin tức theo ID
-        $tin = Tin::find($id);
-        
-        
-        if (!$tin) {
-            return response()->json(['message' => 'Tin tức không tồn tại'], 404);
+        // Kiểm tra ID hợp lệ (phải là số nguyên dương)
+        if (!is_numeric($id) || $id <= 0) {
+            return response()->json([
+                'success' => false,
+                'message' => 'ID không hợp lệ'
+            ], 400);
         }
 
-        
-        return response()->json($tin);
+        // Tìm tin tức theo ID
+        $tin = Tin::find($id);
+
+        // Kiểm tra nếu không tìm thấy tin tức
+        if (!$tin) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Tin tức không tồn tại'
+            ], 404);
+        }
+
+        // Trả về JSON chứa thông tin tin tức
+        return response()->json([
+            'success' => true,
+            'data' => $tin
+        ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -4,12 +4,12 @@ use App\Http\Controllers\ReturnDanhSachController;
 use App\Http\Controllers\TinController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\NhomTinController;
+use App\Http\Controllers\API\ChiTietTinTheoIDController;
 
 
 Route::get('/trangchu', [ReturnDanhSachController::class, 'getDuLieuChoTrangChu']);
 Route::get('/tintuc', [TinController::class, 'danhSachTin']);
 Route::get('/tin-tuc/{id}', [ChiTietTinTheoIDController::class, 'layChiTietTin']);
-
 
 
 //api nhom tin
