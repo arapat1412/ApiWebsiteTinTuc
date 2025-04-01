5796ede12e9fb4c5bcefe117f45fce58389c0015
Hua Vinh Thang <thanghuavinh@gmail.com>
2025-04-01 12:33:21 +0700

    push tincontroller

--- app/Http/Controllers/TinController.php
+++ app/Http/Controllers/TinController.php
@@ -3,31 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\Models\Tin;
+use App\Models\TinTuc;
 use Illuminate\Http\Request;
 
-class TinController extends Controller
+class TinController
 {
-    public function danhSachTin(Request $request)
+    public function danhSachTin()
     {
-        $idNhomTin = $request->query('id_nhomtin');
-        $idLoaiTin = $request->query('id_loaitin');
-
-        $query = Tin::query();
-
-        // Lọc theo loại tin
-        if ($idLoaiTin) {
-            $query->where('id_loaitin', $idLoaiTin);
-        }
-
-        // Lọc theo nhóm tin (thông qua loại tin)
-        if ($idNhomTin) {
-            $query->whereHas('loaiTin', function($q) use ($idNhomTin) {
-                $q->where('id_nhomtin', $idNhomTin);
-            });
-        }
-
         // Lấy danh sách tin
-        $tinTuc = $query->get();
+        $tinTuc = TinTuc::all();
 
         return response()->json($tinTuc);
     }
